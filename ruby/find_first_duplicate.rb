e6f73eea583325845f8315ebed3b406371ed22ef
MZidar <MZidar@gmail.com>
2021-11-03 21:03:13 -0400

    Done

--- ruby/find_first_duplicate.rb
+++ ruby/find_first_duplicate.rb
@@ -1,5 +1,17 @@
 def find_first_duplicate(arr)
-  # type your code in here
+  mod = []
+  arr.each do |entry|
+    mod.each do |check|
+      if check == entry
+        return entry
+      end
+    
+    end
+    mod << entry
+  end
+  -1
+
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -11,8 +23,15 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: -1"
   puts "=>", find_first_duplicate([1, 2, 3, 4])
 
+  puts "Expecting: 2"
+  puts "=>", find_first_duplicate([2, 2, 3, 3])
+
+
   # Don't forget to add your own!
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+# I know I can do this, but don't want to do it too lengthy, also add a code to make sure gets first duplicate
+# I take one element, check it against the rest, but to make it not super lengthy--I compare it to a smaller array, so not duplicating what I look at
+
